8cbf8e0c454f50edc5ece99c96bdb733a6a96f8e
TonyBrowne02 <91893280+TonyBrowne02@users.noreply.github.com>
2023-03-30 22:28:31 +0100

    Update 2
    Implemented a sorting system to return the sorted list of words. String splitter not working, returning blank strings? Will fix soon. Added .txt files of some sample topics, information taken from wikipedia

--- App.java
+++ App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        FileManager polarBear = new FileManager();
+
+        System.out.println(polarBear.CountWords("PolarBear.txt"));
     }
 }
--- FileManager.java
+++ FileManager.java
@@ -9,21 +9,20 @@ import java.util.*;
 
 public class FileManager 
 {
-    public String[] CountWords(String file)
+    public List<Map.Entry<String, Integer>> CountWords(String file)
     {
         String line = "";
-        int i = 0;
-        String[] topwords = new String[10];
-
+        int i,j = 0;
+        
         //make a hashmap mapping the key of a word to an integer value
         Map<String, Integer> wordCount = new HashMap<>();
         try 
         {
             BufferedReader reader = new BufferedReader(new FileReader(file));//create a new reader for first file
-
+            
             while (reader.readLine() != null) //while the next line isnt't null, i.e the end of the file hasn't been reached
             {
-                String[] words = line.split(" [^ a-z A-Z ]+ ");//split the String by non alphabet chars 
+                String[] words = line.split(" [^ a-z A-Z ]+ ");//split the String by non alphabet chars //TODO THIS DOESNT WORK
                 for (String curword : words) //for each String called curword within the array of words within words
                 {
                     if (curword.length() >= 1 ) 
@@ -41,22 +40,29 @@ public class FileManager
                 }//end for each
             }//end while
             reader.close();
-
         }//end try
         catch (Exception e) 
         {
             e.printStackTrace();
         }//end catch
 
+
         List<Map.Entry<String, Integer> > sortedList = new ArrayList<>(wordCount.entrySet());
         //Creates a new list of map entries from the hashmap created above
-        for(i=0; i<(wordCount.size()); i++)
+        for(i=0; i<10; i++)
         {
-
-        }//end for
+            for(j=i+1; j<wordCount.size(); j++)
+            {
+                if(sortedList.get(i).getValue() < sortedList.get(j).getValue())
+                {
+                    Collections.swap(sortedList, i,j);
+                }//end if
+            }//end for
+        }//end outer for
+        //sorts the largest 10 elements to the start of the list, index's 0->9
         
 
-        return ;
+        return sortedList;
     }
 }
 
