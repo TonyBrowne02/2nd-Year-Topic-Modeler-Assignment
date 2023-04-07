1efcd14c397fae4d6026dbed97ffac9e864bc2bf
TonyBrowne02 <91893280+TonyBrowne02@users.noreply.github.com>
2023-04-07 17:17:00 +0100

    Add files via upload

--- App.java
+++ App.java
@@ -4,10 +4,4 @@ public class App {
         new FileSelectorGUI();
 
     }
-    //TODO check for whitespace after a banned word from the text area
-    /*
-         Improving the GUI quality so that it looks better than a very basic GUI (e.g. layout, 
-        colour etc)
-         Extra features that you think will enhance the application.
-     */
 }
